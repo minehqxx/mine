f8f02f7086f0b1c45946ef6c949295b0ea756e2a
Johann Baudy <johaahn@gnu-log.net>
2010-05-30 23:56:58 +0200

    Added missing strrchr.c

---
+++ src/libs/System/string/strrchr.c
@@ -0,0 +1,37 @@
+/*
+ * Wrapper System library
+ *
+ * Copyright (C) 2010 <Johann Baudy> johann.baudy@gnu-log.net
+ * Copyright (C) 2010 <Benoit Gschwind> gschwind@gnu-log.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+/***********************************************************************
+ * Includes
+ ***********************************************************************/
+#include <mine/common.h>
+/* Darwin headers */
+#include <System/string.h>
+/* Glibc Linux headers */
+#include <string.h>
+/***********************************************************************
+ * Functions
+ ***********************************************************************/
+
+MINEAPI char * mine_strrchr(const char * p, int ch) {
+	debug__("(%p,%d)",p, ch);
+	return strrchr(p, ch);
+}
